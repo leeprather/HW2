eb05646f48ea7cfacdd977d6ba2cb13ce9e30903
leeprather <52171089+leeprather@users.noreply.github.com>
2020-02-18 21:24:07 -0500

    Delete CategorySorter.java

--- CategorySorter.java
+++
@@ -1,7 +0,0 @@
-import java.util.Comparator;
-public class CategorySorter implements Comparator<OutputNumber>  {
-	    @Override
-	    public int compare(OutputNumber o1, OutputNumber o2) {
-	        return o1.getCategory().compareTo(o2.getCategory());
-	    }
-}
